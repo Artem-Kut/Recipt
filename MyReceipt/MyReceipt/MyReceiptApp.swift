4ea7dfe33fb66bbee2b9efe6e1d4e4d7531090f6
Artem-Kut <artem.kutasevych@gmail.com>
2024-06-07 10:29:10 -0400

    Create project

---
+++ MyReceipt/MyReceipt/MyReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReceiptApp.swift
+//  MyReceipt
+//
+//  Created by Artem Kutasevych on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MyReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
